c0955e1f7fd84e0bd13e5f51a60e4f2ba3b869c7
danRetama <dan@retama.co.il>
2022-06-29 13:24:15 +0300

    BasketDTO, SubscriberDTO

---
+++ MarketLib/DataAccess/DataTransferObjects/BasketDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DataTransferObjects
+{
+    public class BasketDTO
+    {
+        private int storeid;
+        private ConcurrentDictionary<ProductDTO, int> products; 
+        public ConcurrentDictionary<ProductDTO, int> Products;
+    }
+}
---
+++ MarketLib/DataAccess/DataTransferObjects/SubscriberDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DataTransferObjects
+{
+    public class SubscriberDTO
+    {
+        private int id;// will be used fo synchronization (deadlocks).
+        private bool isAdmin = false;
+        private static int atomicNum;
+        private string userName;
+        private HashSet<StorePermission.StorePermission> permissions; // synchronized manually
+        private ConcurrentDictionary<int, ProductDTO> itemsPurchased; // k: store v: arrylist<product>
+        private ArrayList purchaseHistory;
+    }
+}
